d4de5560e4895787f2c2b83124b7c7f5bc7c2065
Ryan Groesch <ryan.groesch@gmail.com>
2018-05-14 18:46:28 -0700

    Tabs updated

--- src/__test__/lib/user-mock.js
+++ src/__test__/lib/user-mock.js
@@ -5,31 +5,31 @@ import User from '../../model/user';
 import Profile from '../../model/profile';
 
 const createUserMockProm = () => {
- const mock = {};
- mock.request = {
-   username: faker.internet.userName(),
-   email: faker.internet.email(),
-   password: faker.lorem.words(2),
- };
+  const mock = {};
+  mock.request = {
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.lorem.words(2),
+  };
 
- return User.create(mock.request.username, mock.request.password, mock.request.email)
-   .then((user) => {
-     mock.user = user;
-     return user.createTokenProm();
-   })
-   .then((token) => {
-     mock.token = token;
-     return User.findById(mock.user._id);
-   })
-   .then((user) => {
-     mock.user = user;
-     return mock;
-   });
-};
+  return User.create(mock.request.username, mock.request.password, mock.request.email)
+    .then((user) => {
+      mock.user = user;
+      return user.createTokenProm();
+    })
+    .then((token) => {
+      mock.token = token;
+      return User.findById(mock.user._id);
+    })
+    .then((user) => {
+      mock.user = user;
+      return mock;
+    });
+  };
 
 const removeUserMockProm = () => Promise.all([
- User.remove({}),
- Profile.remove({}),
+  User.remove({}),
+  Profile.remove({}),
 ]);
 
 export { createUserMockProm, removeUserMockProm };
--- src/__test__/user-route.test.js
+++ src/__test__/user-route.test.js
@@ -7,56 +7,56 @@ import { createUserMockProm, removeUserMockProm } from './lib/user-mock';
 const apiURL = `http://localhost:${process.env.PORT}`;
 
 describe('USER ROUTER', () => {
- beforeAll(startServer);
- afterEach(removeUserMockProm);
- afterAll(stopServer);
+  beforeAll(startServer);
+  afterEach(removeUserMockProm);
+  afterAll(stopServer);
 
- test('POST creating a user should respond with 200 and a token if there are no errors', () => {
-   return superagent.post(`${apiURL}/signup`)
-     .send({
-       username: 'bob',
-       password: 'this is a secret',
-       email: 'bob@gmail.com',
-     })
-     .then((response) => {
-       expect(response.status).toEqual(200);
-       expect(response.body.token).toBeTruthy();
-     });
- });
- test('POST /signup - an incomplete request should return a 400', () => {
-   return superagent.post(`${apiURL}/signup`)
-     .send({
-       username: 'blob',
-       password: 'this is a secret thing',
-       email: '',
-     })
-     .then(Promise.reject)
-     .catch((response) => {
-       expect(response.status).toEqual(400);
-     });
- });
- describe('GET /login', () => {
-   test('GET /login should get a 200 status code and a token if there are no errors', () => {
-     return createUserMockProm()
-       .then((mock) => {
-         return superagent.get(`${apiURL}/login`)
-           .auth(mock.request.username, mock.request.password);
-       })
-       .then((response) => {
-         expect(response.status).toEqual(200);
-         expect(response.body.token).toBeTruthy();
-       });
-   });
-   test('GET /login should get a 401 for unauthorized access', () => {
-     return createUserMockProm()
-       .then((mock) => {
-         return superagent.get(`${apiURL}/login`)
-           .auth(mock.request.username, 'seth"spassword');
-       })
-       .then(Promise.reject)
-       .catch((response) => {
-         expect(response.status).toEqual(401);
-       });
-   });
- });
+  test('POST creating a user should respond with 200 and a token if there are no errors', () => {
+    return superagent.post(`${apiURL}/signup`)
+      .send({
+        username: 'bob',
+        password: 'this is a secret',
+        email: 'bob@gmail.com',
+      })
+      .then((response) => {
+        expect(response.status).toEqual(200);
+        expect(response.body.token).toBeTruthy();
+      });
+  });
+  test('POST /signup - an incomplete request should return a 400', () => {
+    return superagent.post(`${apiURL}/signup`)
+      .send({
+        username: 'blob',
+        password: 'this is a secret thing',
+        email: '',
+      })
+      .then(Promise.reject)
+      .catch((response) => {
+        expect(response.status).toEqual(400);
+      });
+  });
+  describe('GET /login', () => {
+    test('GET /login should get a 200 status code and a token if there are no errors', () => {
+      return createUserMockProm()
+        .then((mock) => {
+          return superagent.get(`${apiURL}/login`)
+            .auth(mock.request.username, mock.request.password);
+        })
+        .then((response) => {
+          expect(response.status).toEqual(200);
+          expect(response.body.token).toBeTruthy();
+        });
+    });
+    test('GET /login should get a 401 for unauthorized access', () => {
+      return createUserMockProm()
+        .then((mock) => {
+          return superagent.get(`${apiURL}/login`)
+            .auth(mock.request.username, 'seth"spassword');
+        })
+        .then(Promise.reject)
+        .catch((response) => {
+          expect(response.status).toEqual(401);
+        });
+    });
+  });
 });
