114bb3b7665b1d6bb0330714a8f91cefe6aad53b
Ryan Groesch <ryan.groesch@gmail.com>
2018-05-15 09:03:36 -0700

    fixing spacing issues

--- src/route/user-route.js
+++ src/route/user-route.js
@@ -20,29 +20,29 @@ const jsonParser = json();
 
 // POST Route for signing up a new user to our DB
 userRouter.post('/signup', jsonParser, (request, response, next) => {
- return User.create(request.body.username, request.body.password, request.body.email)
-   .then((user) => {
-     delete request.body.password;
-     logger.log(logger.INFO, 'USER - creating a TOKEN HERE');
-     new Profile({ username: request.body.username, user: user._id }).save();
-     return user.createTokenProm();
-   })
-   .then((token) => {
-     logger.log(logger.INFO, 'USER - 200 code and a Token');
-     return response.json({ token });
-   })
-   .then()
-   .catch(next);
+  return User.create(request.body.username, request.body.password, request.body.email)
+    .then((user) => {
+      delete request.body.password;
+      logger.log(logger.INFO, 'USER - creating a TOKEN HERE');
+      new Profile({ username: request.body.username, user: user._id }).save();
+      return user.createTokenProm();
+    })
+    .then((token) => {
+      logger.log(logger.INFO, 'USER - 200 code and a Token');
+      return response.json({ token });
+    })
+    .then()
+    .catch(next);
 });
 
 // login - GET
 userRouter.get('/login', basicAuthMiddleware, (request, response, next) => {
- if (!request.user) {
-   return next(new HttpError(404, 'ERROR user not found'));
- }
- return request.user.createTokenProm()
-   .then(token => response.json({ token }))
-   .catch(next);
+  if (!request.user) {
+    return next(new HttpError(404, 'ERROR user not found'));
+  }
+  return request.user.createTokenProm()
+    .then(token => response.json({ token }))
+    .catch(next);
 });
 
 export default userRouter;
